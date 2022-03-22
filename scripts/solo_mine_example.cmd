8fe6eba24eeec450059d9a50d877eee36f5b4be7
silverwaffle <73851799+silverwaffle@users.noreply.github.com>
2022-03-22 22:07:57 +0100

    Update solo_mine_example.cmd
    donations are helpful

--- scripts/solo_mine_example.cmd
+++ scripts/solo_mine_example.cmd
@@ -12,5 +12,5 @@
 :: But you will only get a payout when you find a block which can take more than a year for a single low-end PC.
 
 cd %~dp0
-xmrig.exe -o node.xmr.to:18081 -a rx/0 -u 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD --daemon
+xmrig.exe -o node.xmr.to:18081 -a rx/0 -u 47bAgvrBJdwAzQuiUBnLmHForC3JLXdYNUqZvKVmoPqSix2Pr7LfCbjQmevLfGfxva7Dksb4UbRwVTN67WsAzqW7RScD8uC --daemon
 pause
